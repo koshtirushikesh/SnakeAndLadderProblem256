dc3c976025647f484a8f0cfc07d0090456ddb6d3
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-09 12:57:01 +0530

    [Rushi]:Added player 2 with if Player gets a Ladder then plays again.

--- SnakeAndLader/Player.cs
+++ SnakeAndLader/Player.cs
@@ -14,32 +14,49 @@ namespace SnakeAndLader
         {
             Random random = new Random();
             int DieRoll = random.Next(1, 7);
-        
+            Console.WriteLine("Player Possition: " + PlayerPossition);
             DieCount++;
+
             PlayerPossition += DieRoll;
             Console.WriteLine("Die Rolled No:" + DieRoll + "\nPlayer Possition : " + PlayerPossition);
 
-            int Option=random.Next(0, 3);
+            int Option =random.Next(0, 3);
 
             if(PlayerPossition<100)
             {
                 switch (Option)
                 {
-                    //no play
-                    case 0: Console.WriteLine("No Play :\nPlayer Possition " + PlayerPossition); break;
-                    case 1:  //lader
+                           //no play
+                    case 0:Console.WriteLine("No Play :"); break;
+
+                    case 1:  /*lader*/
                         PlayerPossition += DieRoll;
-                        Console.WriteLine("lader : \nPlayer Possition " + PlayerPossition); break;
-                    case 2: // snake
+                        Console.WriteLine("lader :");
+                        if (PlayerPossition < 100)
+                        {
+                            Console.WriteLine("Player Possition :" + PlayerPossition);
+                            Console.WriteLine("Since Player got lader so player get one more chance:");
+                            Playing(PlayerPossition);
+                        }
+                        break;
+                    case 2: /* snake*/ 
                         PlayerPossition -= DieRoll;
-                        Console.WriteLine("Snake : \nPlayer Possition " + PlayerPossition); break;
+                        Console.WriteLine("Snake : ");
+                        Console.WriteLine("Player Possition :" + PlayerPossition);
+                        break;
                 }
             }
 
             if (PlayerPossition < 0)
+            {
                 PlayerPossition = 0;
-            else if (PlayerPossition > 100) 
+                Console.WriteLine("player going below so restarting from :" + PlayerPossition);
+            } 
+            else if (PlayerPossition > 100)
+            {
                 PlayerPossition -= DieRoll;
+                Console.WriteLine("player going above 100 so start from same place : " + PlayerPossition);
+            }
 
             return PlayerPossition;
         }
--- SnakeAndLader/Program.cs
+++ SnakeAndLader/Program.cs
@@ -12,25 +12,26 @@ namespace SnakeAndLader
         {
             Player player1 = new Player();
             Player player2 = new Player();
-            int PlayerPossition_1 = 0,PlayerPossition_2 = 0;
-            Console.WriteLine("Player 1 Possition: "+player1.PlayerPossition);
-            Console.WriteLine("Player 2 Possition: "+player2.PlayerPossition);
-            
 
+            int PlayerPossition_1 = 0,PlayerPossition_2 = 0;
+            Console.WriteLine("Enter the player 1 name ");
+            string player_One= Console.ReadLine();
+
+            Console.WriteLine("Enter the player 2 name ");
+            string player_Two= Console.ReadLine();
+            
             while (PlayerPossition_1 < 120 && PlayerPossition_2 < 120)
             {
-
-                Console.WriteLine("-------------------- Player 1 Playing ");
+                
+                Console.WriteLine($"-------------------- {player_One} Playing ");
                 PlayerPossition_1 = player1.Playing(PlayerPossition_1);
-
                 if (PlayerPossition_1 == 100)
                 {
                     Console.WriteLine("Player 1 Win"); break;
                 }
-
-                Console.WriteLine("-------------------- Player 2 Playing ");
+                
+                Console.WriteLine($"-------------------- {player_Two} Playing ");
                 PlayerPossition_2 = player2.Playing(PlayerPossition_2);
-
                 if (PlayerPossition_2 == 100)
                 {
                     Console.WriteLine("Player 2 Win"); break;
